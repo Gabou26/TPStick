c7c9ff87832b6d01e77319f53a33fc9fec3b4102
Léo Vansimay <leo.vansimay@insa-cvl.fr>
2021-11-03 11:46:11 -0400

    ajout de la fonction de saut, pas de bug observé après de nouveaux tests, modification des controles camera pour correspondre à un jeu de tir

--- Assets/Scripts/Third_person_mvmnt.cs
+++ Assets/Scripts/Third_person_mvmnt.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Third_person_mvmnt : MonoBehaviour
 {
     public CharacterController controller;
 
     public float speed = 6f;
+    public float jumpForce;
+    public float gravityScale;
+    public float yvelocity;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    public Transform camera;
+    
     
 
     // Update is called once per frame
@@ -17,15 +24,31 @@ public class Third_person_mvmnt : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
+        //Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 direction = (camera.forward * vertical) + (camera.right * horizontal); 
+        direction.Normalize();
+        direction = direction * speed;
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             
-            controller.Move(direction * speed * Time.deltaTime);
+            
         }
+
+        
+        if (controller.isGrounded)
+        {
+            yvelocity = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                yvelocity = jumpForce;
+            }
+        }
+        yvelocity += Physics.gravity.y * Time.deltaTime;
+        direction.y = yvelocity;
+        controller.Move( direction * Time.deltaTime);
+        
     }
 }
