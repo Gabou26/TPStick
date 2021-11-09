7c21d97ce1bf9c99dc5c26d6a87ea51055cde889
Quentin <quentin.callens@student.junia.com>
2021-11-08 19:30:43 -0500

    Update SpawnCrates.cs
    Random material for each obect

--- Assets/SpawnCrates.cs
+++ Assets/SpawnCrates.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class SpawnCrates : MonoBehaviour
 {
     public Material cubeMat;
+    private Material cubeMaterial;
+    private int nbMaterial;
+    public List<Material> unityGameObjects = new List<Material>();
     [SerializeField] private Transform objectsSpawnZone;
     float min;
     float max;
@@ -40,12 +43,27 @@ public class SpawnCrates : MonoBehaviour
         //Change o� va spawn ce cube
         //cube.transform.position = new Vector3(0, 0.5f, 0);
         cube.transform.position = spawnPoint;
-
-
-        cube.GetComponent<Renderer>().material = cubeMat;
-
+        nbMaterial = unityGameObjects.Count;
+        cubeMaterial = unityGameObjects[Random.Range(0,nbMaterial)];
+        cube.GetComponent<Renderer>().material = cubeMaterial;
+        cube.GetComponent<Rigidbody>().mass = 10000.0f;
+        cube.GetComponent<Rigidbody>().isKinematic = false;
         Destroy(cube, 20.0f);
-
+        //StartCoroutine(ChangeMaterial(cube,0.1));
+        //Invoke("ChangeMaterial", 0.1f);
         Invoke("SpawnBoxes", Random.Range(min,max));
     }
+
+    void ChangeMaterial(GameObject cube,double delayTime){
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+    }
+
 }
