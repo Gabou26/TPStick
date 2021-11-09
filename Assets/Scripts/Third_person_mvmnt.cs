a69defa18d81223591b537a21893b32037403871
Quentin <quentin.callens@student.junia.com>
2021-11-09 08:24:03 -0500

    Add code for several power

--- Assets/Scripts/Third_person_mvmnt.cs
+++ Assets/Scripts/Third_person_mvmnt.cs
@@ -174,8 +174,29 @@ public class Third_person_mvmnt : MonoBehaviour
         if(powerName == "SpeedUp"){
             speed = 30f;
         }
-        else{
+        else if(powerName == "SpeedDown"){
             speed = 20f;
+        }
+        else if(powerName == "SpeedDown"){
+            speed = 20f;
+        }
+        else if(powerName == "ArmorUp"){
+            speed = 20f;
+        }
+        else if(powerName == "ArmorDown"){
+            speed = 20f;
+        }
+        else if(powerName == "AttackUp"){
+            speed = 20f;
+        }
+        else if(powerName == "AttackDown"){
+            speed = 20f;
+        }
+        else if(powerName == "changeGuns"){
+            speed = 20f;
+        }
+        else{
+            speed = 6f;
         }
     }
     void initialisePlayerProperties(){
