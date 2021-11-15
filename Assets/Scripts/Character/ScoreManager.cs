24080111f6ca3321ef82cdf09207132957b58e13
Léo Vansimay <leo.vansimay@insa-cvl.fr>
2021-11-15 18:03:49 -0500

    mise à jour de la branche, début du travail sur le système de score.

---
+++ Assets/Scripts/Character/ScoreManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    
+
+    public int scoreBuffer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
