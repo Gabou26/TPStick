bf2b8d85033cf5e8b8707bb443d8b6bf01eb2cb8
Quentin <quentin.callens@student.junia.com>
2021-11-21 11:37:33 -0500

    Mofify current power image

--- Assets/activePowerImage.cs
+++ Assets/activePowerImage.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class activePowerImage : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
+    public Image speedImages;
+    public Image armorImages;
+    public Image attackImages;
     public Sprite[] spritePowerList;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        
         //Invoke("ChangeSprite", 10f);
     }
 
@@ -19,13 +23,22 @@ public class activePowerImage : MonoBehaviour
     {
     }
 
-    public void ChangeSprite(Color color)
+    public void ChangeSpriteAttack(string motCle)
     {
-        spriteRenderer.color = color; 
+        if(motCle == "Up") attackImages.sprite = spritePowerList[0];
+        if(motCle == "Down") attackImages.sprite = spritePowerList[1];
+        if(motCle == "reset") attackImages.sprite = spritePowerList[2];
     }
-
-    void ChangeSprite()
+    public void ChangeSpriteSpeed(string motCle)
     {
-        spriteRenderer.color = Color.black; 
+        if(motCle == "Up") speedImages.sprite = spritePowerList[0];
+        if(motCle == "Down") speedImages.sprite = spritePowerList[1];
+        if(motCle == "reset") speedImages.sprite = spritePowerList[2];
+    }
+    public void ChangeSpriteArmor(string motCle)
+    {
+        if(motCle == "Up") armorImages.sprite = spritePowerList[0];
+        if(motCle == "Down") armorImages.sprite = spritePowerList[1];
+        if(motCle == "reset") armorImages.sprite = spritePowerList[2]; 
     }
 }
