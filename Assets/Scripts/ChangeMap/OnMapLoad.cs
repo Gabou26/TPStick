072d075996a2b2ccbb9a02bf9da06d0168a6bf14
Hevy1 <evan.weisse@outlook.fr>
2021-11-30 18:12:06 -0500

    Supression des debug.log

--- Assets/Scripts/ChangeMap/OnMapLoad.cs
+++ Assets/Scripts/ChangeMap/OnMapLoad.cs
@@ -20,7 +20,6 @@ public class OnMapLoad : MonoBehaviour
         for (int i = 0; i < len; i++) {
             float x = Convert.ToSingle(20*Math.Cos(2*Math.PI*i/len));
             float z = Convert.ToSingle(20*Math.Sin(2*Math.PI*i/len));
-            Debug.Log("Respawning player " + i + " at " + x + ", 15, " + z);
             players[i].transform.position = new Vector3(x , 15, z);
         }
     }
