2741133c3ea769a629f83a3614ae73eb8b9bbcac
Léo Vansimay <l.vansimay@gmail.com>
2021-12-02 15:37:10 -0500

    removed unused imports

--- Assets/Scripts/Pause/NewPause.cs
+++ Assets/Scripts/Pause/NewPause.cs
@@ -1,10 +1,6 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class NewPause : MonoBehaviour
 {
