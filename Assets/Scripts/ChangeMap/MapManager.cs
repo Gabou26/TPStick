f61530e8c79db14c8e13ec54da9ba91e3930c20f
Hevy1 <evan.weisse@outlook.fr>
2021-12-06 16:59:45 -0500

    Fin du MapManager + Fin des Maps 2 et 5 + Ajout des skyboxs

--- Assets/Scripts/ChangeMap/MapManager.cs
+++ Assets/Scripts/ChangeMap/MapManager.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
     public ChangeMapTrigger map1, map2, map3, map4, map5;
     public PlayerInputManager manager;
+    public Text countText;
 
     public void Start() {
         manager = GameObject.FindObjectOfType<PlayerInputManager>();
@@ -18,10 +19,23 @@ public class MapManager : MonoBehaviour
     {
         int[] counts = {map1.playerCount, map2.playerCount, map3.playerCount, map4.playerCount,  map5.playerCount};
         string[] maps = {map1.mapName, map2.mapName, map3.mapName, map4.mapName,  map5.mapName};
-        int max = counts.Max();
+        int max = maxTab(counts);
         if (counts[max] > 0) {
-            string message = maps[max] + " : " + counts[max] + "/" + manager.playerCount;
-            Debug.Log(message);
+            countText.text = maps[max] + " : " + counts[max] + "/" + manager.playerCount;
+            Debug.Log("Setting text");
+        } else {
+            countText.text = "";
+            Debug.Log("Deleting text");
         }
+    }
+
+    private int maxTab(int[] tab) {
+        int m = 0;
+        for (int i=0; i<tab.Length; i++) {
+            if (tab[i] > tab[m]) {
+                m = i;
+            }
+        }
+        return m;
     }
 }
